c7cd27ee8ad3d0388fafa305a44f7c55b3fab2e3
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-02-13 11:06:47 +0300

    created and styled header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ["Mea Culpa"],
+      },
+      colors:{
+        "babypink":"#eec4c6",
+        "cherry":"#791429",
+        "darkcherry":"#460d19",
+        "cherryDarkmode":"#1a1213",  
+        "babypinkDarkmode":"#402d31"
+          
+         }
+    },
   },
   plugins: [],
-}
-
+};
