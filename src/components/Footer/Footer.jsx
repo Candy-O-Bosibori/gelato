ba926a18fba490db4ef947b10f7011fb121dbdc7
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-02-13 11:19:50 +0300

    created and styled footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div>
-      Footer
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-20 text-babypink flex items-center justify-between bg-cherry">
+      <Link href="home page" className="font-logo  font-bold text-3xl" > Gelato</Link>
+      <p> © Copyright// all rifghts reserved</p>
     </div>
   )
 }
