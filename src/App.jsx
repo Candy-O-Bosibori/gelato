87c3ee5ff71fdf85268b50153a04b49a992806b0
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-02-15 23:36:22 +0300

    5

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,55 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import FirstPage from "./components/FirstPage/FirstPage";
-import { ProtectedRoute } from './components/ProtectedRoute';
-import { useState , useEffect} from 'react'
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Home from './components/Home/Home';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './firebase';
-
-
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { useState, useEffect } from "react";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Home from "./components/Home/Home";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
-const [user, setUser] = useState(null);
-const [isfetching, setIsfetching] = useState(true)
-useEffect (()=> {
-  const unsubscribe = onAuthStateChanged(auth, (user)=> {
-    if(user){
-      setUser(user);
-      setIsfetching(false)
-      return;
-    }
-    setUser(null)
-    setIsfetching(false)
-  })
-  return()=> unsubscribe()
-}, [])
+  const [user, setUser] = useState(null);
+  const [isfetching, setIsfetching] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setIsfetching(false);
+        return;
+      }
+      setUser(null);
+      setIsfetching(false);
+    });
+    return () => unsubscribe();
+  }, []);
 
- if(isfetching){
-  return <h2>Loading...</h2>
- }
+  if (isfetching) {
+    return <h2>Loading...</h2>;
+  }
 
   return (
     <>
-    <BrowserRouter  basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}>
-    <Routes >
-       <Route index path='/' element={<FirstPage></FirstPage>}></Route>
-       <Route index path='register' element={<Register></Register>}></Route>
-       <Route index path='login' element={<Login></Login>}></Route>
-       <Route index path='/private' element={<ProtectedRoute user={user}>
-        <Home></Home>
-       </ProtectedRoute>}></Route>
-
-
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter
+        basename={import.meta.env.DEV ? "/" : "/gelato/"}
+      >
+        <Routes>
+          <Route index path="/" element={<FirstPage></FirstPage>}></Route>
+          <Route index path="register" element={<Register></Register>}></Route>
+          <Route index path="login" element={<Login></Login>}></Route>
+          <Route
+            index
+            path="/private"
+            element={
+              <ProtectedRoute user={user}>
+                <Home></Home>
+              </ProtectedRoute>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
